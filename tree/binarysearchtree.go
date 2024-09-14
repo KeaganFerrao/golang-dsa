c04b6ebcd54974a9fdc1cc66d054b7f0a648b797
keagan-Ferrao <keagan@osplabs.com>
2024-09-14 19:05:58 +0530

    Updates in bst

--- tree/binarysearchtree.go
+++ tree/binarysearchtree.go
@@ -1,9 +1,6 @@
 package tree
 
-import (
-	"cmp"
-	"fmt"
-)
+import "cmp"
 
 type binarySearchTree[T cmp.Ordered] struct {
 	Root *Node[T]
@@ -13,196 +10,184 @@ func NewBinarySearchTree[T cmp.Ordered]() *binarySearchTree[T] {
 	return &binarySearchTree[T]{}
 }
 
-func (bt *binarySearchTree[T]) PreOrderTraversal(node *Node[T]) {
+func (bst *binarySearchTree[T]) InOrderTraversal(node *Node[T], cb func(*Node[T])) {
 	if node == nil {
 		return
 	}
 
-	fmt.Printf("Data: %v\n", node.Data)
-
-	bt.PreOrderTraversal(node.Left)
-	bt.PreOrderTraversal(node.Right)
+	bst.InOrderTraversal(node.Left, cb)
+	cb(node)
+	bst.InOrderTraversal(node.Right, cb)
 }
 
-func (bt *binarySearchTree[T]) InOrderTraversal(node *Node[T]) {
+func (bst *binarySearchTree[T]) PreOrderTraversal(node *Node[T], cb func(*Node[T])) {
 	if node == nil {
 		return
 	}
 
-	bt.InOrderTraversal(node.Left)
-	fmt.Printf("Data: %v\n", node.Data)
-	bt.InOrderTraversal(node.Right)
+	cb(node)
+	bst.PreOrderTraversal(node.Left, cb)
+	bst.PreOrderTraversal(node.Right, cb)
 }
 
-func (bt *binarySearchTree[T]) PostOrderTraversal(node *Node[T]) {
+func (bst *binarySearchTree[T]) PostOrderTraversal(node *Node[T], cb func(*Node[T])) {
 	if node == nil {
 		return
 	}
 
-	bt.InOrderTraversal(node.Left)
-	bt.InOrderTraversal(node.Right)
-
-	fmt.Printf("Data: %v\n", node.Data)
+	bst.PostOrderTraversal(node.Left, cb)
+	bst.PostOrderTraversal(node.Right, cb)
+	cb(node)
 }
 
-func (bt *binarySearchTree[T]) Height(node *Node[T]) int {
-	if node == nil {
-		return 0
-	}
-
-	return 1 + max(bt.Height(node.Left), bt.Height(node.Right))
-}
-
-func (bt *binarySearchTree[T]) LevelOrderTraversal(node *Node[T]) {
+func (bst *binarySearchTree[T]) LevelOrderTraversal(node *Node[T], cb func(*Node[T])) {
 	queue := make([]*Node[T], 0)
-
 	queue = append(queue, node)
+
 	for len(queue) > 0 {
-		element := queue[0]
+		temp := queue[0]
+		cb(temp)
 		queue = queue[1:]
-		fmt.Printf("Data: %v\n", element.Data)
 
-		if element.Left != nil {
-			queue = append(queue, element.Left)
+		if temp.Left != nil {
+			queue = append(queue, temp.Left)
 		}
-		if element.Right != nil {
-			queue = append(queue, element.Right)
+		if temp.Right != nil {
+			queue = append(queue, temp.Right)
 		}
 	}
 }
 
-func (bt *binarySearchTree[T]) ReverseLevelOrderTraversal(node *Node[T]) {
-	queue := make([]*Node[T], 0)
-	stack := make([]*Node[T], 0)
-
-	queue = append(queue, node)
-	for len(queue) > 0 {
-		element := queue[0]
-		queue = queue[1:]
-		stack = append(stack, element)
-
-		if element.Left != nil {
-			queue = append(queue, element.Left)
-		}
-		if element.Right != nil {
-			queue = append(queue, element.Right)
-		}
-	}
-
-	for len(stack) > 0 {
-		element := stack[len(stack)-1]
-
-		stack = stack[:len(stack)-1]
-		fmt.Printf("Data: %v\n", element.Data)
-	}
-}
-
-func (bt *binarySearchTree[T]) Size(node *Node[T]) int {
-	if node == nil {
-		return 0
-	}
-
-	return 1 + bt.Size(node.Left) + bt.Size(node.Right)
-}
-
-func (bst *binarySearchTree[T]) Insert(root *Node[T], key T) error {
-	node := &Node[T]{Data: key}
-
+func (bst *binarySearchTree[T]) InsertRecursive(key T, root *Node[T]) *Node[T] {
 	if root == nil {
+		return &Node[T]{Data: key}
+	}
+
+	// No duplicates
+	if key == root.Data {
+		return root
+	}
+
+	if key < root.Data {
+		root.Left = bst.InsertRecursive(key, root.Left)
+	} else if key > root.Data {
+		root.Right = bst.InsertRecursive(key, root.Right)
+	}
+
+	return root
+}
+
+func (bst *binarySearchTree[T]) Insert(key T) {
+	node := &Node[T]{Data: key}
+	if bst.Root == nil {
 		bst.Root = node
-		return nil
+		return
 	}
 
 	var parent *Node[T]
-	curr := root
-
-	//Loop through each node and keep a reference to the parent as well
+	curr := bst.Root
 	for curr != nil {
 		parent = curr
-		if curr.Data > key {
+		if key < curr.Data {
 			curr = curr.Left
-		} else if curr.Data < key {
+		} else if key > curr.Data {
 			curr = curr.Right
 		} else {
-			return fmt.Errorf("Duplicate key %v", key)
+			// No duplicates
+			return
 		}
 	}
 
-	//Parent is the node where we insert
-	if parent.Data > key {
+	if key < parent.Data {
 		parent.Left = node
-	} else {
+	} else if key > parent.Data {
 		parent.Right = node
 	}
+}
 
-	return nil
+func (bst *binarySearchTree[T]) Height(root *Node[T]) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + max(bst.Height(root.Left), bst.Height(root.Right))
+}
+
+func (bst *binarySearchTree[T]) Size(root *Node[T]) int {
+	if root == nil {
+		return 0
+	}
+
+	return 1 + bst.Size(root.Left) + bst.Size(root.Right)
+}
+
+func getInorderSuccessor[T cmp.Ordered](node *Node[T]) *Node[T] {
+	temp := node.Right
+	for temp != nil && temp.Left != nil {
+		temp = temp.Left
+	}
+	return temp
+}
+
+func (bst *binarySearchTree[T]) Delete(key T, node *Node[T]) *Node[T] {
+	if bst.Root == nil {
+		return nil
+	}
+
+	if key < node.Data {
+		node.Left = bst.Delete(key, node.Left)
+	} else if key > node.Data {
+		node.Right = bst.Delete(key, node.Right)
+	} else {
+		if node.Left == nil {
+			return node.Right
+		}
+
+		if node.Right == nil {
+			return node.Left
+		}
+
+		successor := getInorderSuccessor(node)
+		node.Data = successor.Data
+		node.Right = bst.Delete(successor.Data, node.Right)
+	}
+
+	return node
+}
+
+func sortedArrayToBalancedBst[T cmp.Ordered](arr []T, start int, end int) *Node[T] {
+	if start > end {
+		return nil
+	}
+
+	mid := (start + end) / 2
+	node := &Node[T]{Data: arr[mid]}
+
+	node.Left = sortedArrayToBalancedBst(arr, start, mid-1)
+	node.Right = sortedArrayToBalancedBst(arr, mid+1, end)
+
+	return node
+}
+
+func (bst *binarySearchTree[T]) Rebalance() {
+	var arr []T
+
+	bst.InOrderTraversal(bst.Root, func(n *Node[T]) {
+		arr = append(arr, n.Data)
+	})
+
+	bst.Root = sortedArrayToBalancedBst(arr, 0, len(arr)-1)
 }
 
 func (bst *binarySearchTree[T]) Search(root *Node[T], key T) *Node[T] {
-	if root == nil || key == root.Data {
-		return root
+	if root == nil {
+		return nil
 	}
 
 	if key < root.Data {
 		return bst.Search(root.Left, key)
 	} else if key > root.Data {
 		return bst.Search(root.Right, key)
-	}
-
-	return nil
-}
-
-// Inorder successor: The node that comes the immediate next when we follow
-// an inorder traversal => Left, Root, Right
-// We do an inorder traversal, because it always results in nodes in
-// ascending order, so we know which node should we choose to replace the
-// node to delete with
-// Here we only consider right subtree because we are sure that it has a
-// right since we are checking conditions in the caller code.
-func getSuccessor[T cmp.Ordered](node *Node[T]) *Node[T] {
-	node = node.Right
-	for node != nil && node.Left != nil {
-		node = node.Left
-	}
-	return node
-}
-
-func (bst *binarySearchTree[T]) DeleteRecursive(root *Node[T], key T) *Node[T] {
-	if root == nil {
-		return nil
-	}
-
-	if key < root.Data {
-		root.Left = bst.DeleteRecursive(root.Left, key)
-	} else if key > root.Data {
-		root.Right = bst.DeleteRecursive(root.Right, key)
 	} else {
-		// If only single child present
-		if root.Left == nil {
-			return root.Right
-		}
-		// If only single child present
-		if root.Right == nil {
-			return root.Left
-		}
-
-		// If both children present, get the inorder successor
-		// Replace the data in the node with the successor
-		// And delete the successor in the tree, which is
-		// the leftmost node in the right subtree
-		successor := getSuccessor(root)
-		root.Data = successor.Data
-		root.Right = bst.DeleteRecursive(root.Right, successor.Data)
+		return root
 	}
-
-	return root
-}
-
-// func (bst *binarySearchTree[T]) Rebalance() {
-
-// }
-
-func SortedArrayToBalancedBst[T cmp.Ordered](arr []T) *binarySearchTree[T] {
-
-	return nil
 }
